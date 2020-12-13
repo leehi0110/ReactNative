350a16810ed6bb71560a6b71f9b318dedc436fb7
SeungNyungLee <maple0110@naver.com>
2020-12-13 18:48:47 +0900

    [Update] '00' Button Add
    버튼에 00을추가하였다.

--- CustomCalculator/src/Context/Data/index.tsx
+++ CustomCalculator/src/Context/Data/index.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, useState, useEffect} from 'react';
+import React, {createContext, useState} from 'react';
 import * as func from '~/Context/common/common';
 
 const CalContext = createContext<ICalContext>({
